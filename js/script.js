6d0d372270ebd0ca998db79421bf2882253a439d
Rajmeet-Singh78 <106888019+Rajmeet-Singh78@users.noreply.github.com>
2022-06-04 18:30:27 -0400

    Add files via upload
    Added installation and activation logic in Service Worker.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 if ('serviceWorker' in navigator) {
+<<<<<<< HEAD
+    navigator.serviceWorker.register('/service-worker.js', {scope:'/'})
+=======
     navigator.serviceWorker.register('/service-worker.js', {scope : '/'})
+>>>>>>> main
     .then((registration) => {
-    console.log('Success. Scope:', registration.scope);
+    console.log('SW Registered:', registration);
     })
     .catch((error) => {
     console.log('Failed. Error:', error);
--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,22 @@
-self.addEventListener('fetch', () => {
-    return;
+
+// Everytime something comes from the web
+self.addEventListener('fetch', (event) => {
+    return
+    });
+
+
+
+//Called when SW gets installed
+self.addEventListener('install', (event) => {
+    console.log("SW Installed: ", event);
+
+    //To skip the waiting and making SW activated 
+    self.skipWaiting();
+    });
+
+
+// Called when SW gets activated
+self.addEventListener('activate', (event) => {
+    console.log("SW Activated: ", event);
+    event.waitUntil(clients.claim());
     });
