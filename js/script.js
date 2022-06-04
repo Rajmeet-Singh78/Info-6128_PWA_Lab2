abc82b7dda2865d46e035a260f6dee3dc8642a29
Rajmeet-Singh78 <106888019+Rajmeet-Singh78@users.noreply.github.com>
2022-06-04 18:47:41 -0400

    Updated scope

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js', {scope:'/'})
+    navigator.serviceWorker.register('/Info-6128_PWA_Lab2/service-worker.js', {scope:'/Info-6128_PWA_Lab2'})
     .then((registration) => {
     console.log('SW Registered:', registration);
     })
